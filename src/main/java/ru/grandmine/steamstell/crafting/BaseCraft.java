4f77739a91271c71e39ca4cade804f6713d8f1ad
connor41 <primerovmax@gmail.com>
2017-06-08 17:22:29 +0300

    addCraft
    Добавил крафты:
    -Блоку алюминия и слитку
    -Блоку меди и слитку
    -Блоку свинца и слитку
    -Блоку урана и слитку
    -Блоку олова и слитку
    -Блоку рубина и рубину
    -Блоку никеля и слитку
    
    Magic исправь текстурку серы пж

--- src/main/java/ru/grandmine/steamstell/crafting/BaseCraft.java
+++ src/main/java/ru/grandmine/steamstell/crafting/BaseCraft.java
@@ -8,7 +8,27 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class BaseCraft {
 	
 	public static void init() {	
-		addSmelting();	
+		addSmelting();
+		addRecipe();
+	}
+	
+	public static void addRecipe() {
+		
+		GameRegistry.addRecipe( new ItemStack(GrandBlocks.aluminiumblock), new Object[] { "###", "###", "###", '#', GrandItem.aluminiumingot});
+		GameRegistry.addRecipe( new ItemStack(GrandItem.aluminiumingot, 9), new Object[] { "#", '#', GrandBlocks.aluminiumblock});
+		GameRegistry.addRecipe( new ItemStack(GrandBlocks.copperblock), new Object[] { "###", "###", "###", '#', GrandItem.copperingot});
+		GameRegistry.addRecipe( new ItemStack(GrandItem.copperingot, 9), new Object[] { "#", '#', GrandBlocks.copperblock});
+		GameRegistry.addRecipe( new ItemStack(GrandBlocks.leadblock), new Object[] { "###", "###", "###", '#', GrandItem.leadingot});
+		GameRegistry.addRecipe( new ItemStack(GrandItem.leadingot, 9), new Object[] { "#", '#', GrandBlocks.leadblock});
+		GameRegistry.addRecipe( new ItemStack(GrandBlocks.nickelblock), new Object[] { "###", "###", "###", '#', GrandItem.nickelingot});
+		GameRegistry.addRecipe( new ItemStack(GrandItem.nickelingot, 9), new Object[] { "#", '#', GrandBlocks.nickelblock});
+		GameRegistry.addRecipe( new ItemStack(GrandBlocks.rubyblock), new Object[] { "###", "###", "###", '#', GrandItem.ruby});
+		GameRegistry.addRecipe( new ItemStack(GrandItem.ruby, 9), new Object[] { "#", '#', GrandBlocks.rubyblock});
+		GameRegistry.addRecipe( new ItemStack(GrandBlocks.tinblock), new Object[] { "###", "###", "###", '#', GrandItem.tiningot});
+		GameRegistry.addRecipe( new ItemStack(GrandItem.tiningot, 9), new Object[] { "#", '#', GrandBlocks.tinblock});
+		GameRegistry.addRecipe( new ItemStack(GrandBlocks.uraniumblock), new Object[] { "###", "###", "###", '#', GrandItem.uraniumingot});
+		GameRegistry.addRecipe( new ItemStack(GrandItem.uraniumingot, 9), new Object[] { "#", '#', GrandBlocks.uraniumblock});
+		
 	}
 	
 	public static void addSmelting() {
